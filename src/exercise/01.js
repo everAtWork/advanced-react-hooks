78714bb3c45ea4914f2d14ab812b80d8208148ae
everAtWork <ever.at.work@gmail.com>
2021-08-09 15:32:15 +0300

    done extra1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,15 +2,16 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-const countReducer = (state, newState) => {
-  return newState;
+const countReducer = (count, step) => {
+  return count + step;
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
-  const increment = () => setCount(count + step)
+  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+  const increment = () => changeCount(step)
   return <button onClick={increment}>{count}</button>
 }
+
 
 function App() {
   return <Counter />
